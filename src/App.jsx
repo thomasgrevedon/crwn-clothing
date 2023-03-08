b2f9bb32e17c86fbc2347fd0003f69993403277b
Thomas Grevedon <thomas.grevedon@cantor.fr>
2023-03-08 06:12:45 +0100

    feat(react-training): scaffolding project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+const App = () => {
+  return (
+   <div className="categories-container">
+    <div className="category-container">
+      <p>HAT</p>
+      <p>Shop Now!</p>
+    </div>
+    <div className="category-container">
+      <p>Jacket</p>
+      <p>Shop Now!</p>
+    </div>
+    <div className="category-container">
+      <p>Jeans</p>
+      <p>Shop Now!</p>
+    </div>
+    <div className="category-container">
+      <p>Pull over</p>
+      <p>Shop Now!</p>
+    </div>
+    <div className="category-container">
+      <p>Shoes</p>
+      <p>Shop Now!</p>
+    </div>
+   </div>
+  );
+}
+
+export default App;
