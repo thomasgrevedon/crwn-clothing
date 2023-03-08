07efb60deb815a283a3d278b7bcde73c6f134dff
Thomas Grevedon <thomas.grevedon@cantor.fr>
2023-03-08 06:27:56 +0100

    feat(react-training): use map to display an array of categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hat'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    },
+  ]
+
   return (
    <div className="categories-container">
-    <div className="category-container">
-      <p>HAT</p>
-      <p>Shop Now!</p>
-    </div>
-    <div className="category-container">
-      <p>Jacket</p>
-      <p>Shop Now!</p>
-    </div>
-    <div className="category-container">
-      <p>Jeans</p>
-      <p>Shop Now!</p>
-    </div>
-    <div className="category-container">
-      <p>Pull over</p>
-      <p>Shop Now!</p>
-    </div>
-    <div className="category-container">
-      <p>Shoes</p>
-      <p>Shop Now!</p>
-    </div>
+    {
+      categories.map(({id, title}) => {
+      return(<div className="category-container" key={id}>
+            <img className="image-background"/>
+            <p>title</p>
+            <p>Shop Now!</p>
+          </div>)
+      })
+    }
+    
    </div>
   );
 }
